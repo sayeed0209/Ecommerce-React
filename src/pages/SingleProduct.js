a60b7cb54776105e240c16e074ff8a15d8d189ce
sayeed <sayeed0209@gmail.com>
2022-11-27 18:52:12 +0100

    single product page

--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -1,7 +1,128 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import { useProductContext } from '../context/products_context';
+import { formatPrice } from '../utils/helper';
+import PageHero from '../components/PageHero';
+import styled from 'styled-components';
+import LoadingSpinner from '../components/LoadingSpinner';
+import Error from '../components/Error';
+import { API_URL } from '../utils/action.js';
 const SingleProduct = () => {
-  return <div>SingleProduct</div>;
-};
+  const {
+    fetchSingleProducts,
+    single_products_data: product,
+    single_products_loading: loading,
+    single_products_error: error,
+  } = useProductContext();
+  const { id } = useParams();
 
+  useEffect(() => {
+    fetchSingleProducts(`${API_URL}/${id}`);
+  }, [id]);
+
+  if (loading) {
+    return <LoadingSpinner />;
+  }
+  if (error) {
+    return <Error />;
+  }
+  const {
+    imgUrl,
+    model,
+    brand,
+    price,
+    cpu,
+    ram,
+    os,
+    displayResolution,
+    battery,
+    primaryCamera,
+    dimentions,
+    weight,
+  } = product;
+  return (
+    <Wrapper>
+      <PageHero title={product.brand} product />
+      <div className="container mt-5">
+        <Link to="/" className="btn btn-warning text-white">
+          Back To Home
+        </Link>
+        <div className="product-center">
+          <img src={imgUrl} alt={model} />
+          <section className="content">
+            <h2>{brand}</h2>
+            <h5 className="price">{formatPrice(price)}</h5>
+            <p className="info">
+              <span>Model: </span>
+              {model}
+            </p>
+            <p className="info">
+              <span>OS: </span>
+              {os}
+            </p>
+            <p className="info">
+              <span>CPU: </span>
+              {cpu}
+            </p>
+            <p className="info">
+              <span>RAM: </span>
+              {ram}
+            </p>
+            <p className="info">
+              <span>Display resolution: </span>
+              {displayResolution}
+            </p>
+            <p className="info">
+              <span>Battery: </span>
+              {battery}
+            </p>
+            <p className="info">
+              <span>Primary Camera: </span>
+              {primaryCamera}
+            </p>
+            <p className="info">
+              <span>Dimension: </span>
+              {dimentions}
+            </p>
+            <p className="info">
+              <span>Weight: </span>
+              {weight} GM
+            </p>
+            <hr />
+            <button>Add to Cart</button>
+          </section>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
+const Wrapper = styled.main`
+  .product-center {
+    display: grid;
+    gap: 2rem;
+    margin-top: 2rem;
+  }
+  .price {
+    color: #0dcaf0;
+  }
+
+  .info {
+    text-transform: capitalize;
+    width: 300px;
+    display: grid;
+    grid-template-columns: 125px 1fr;
+    span {
+      font-weight: 700;
+    }
+  }
+
+  @media (min-width: 992px) {
+    .product-center {
+      grid-template-columns: 350px 1fr;
+    }
+    .price {
+      font-size: 1.25rem;
+    }
+  }
+`;
 export default SingleProduct;
