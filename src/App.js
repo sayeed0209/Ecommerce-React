e70d510db2b487ed43d36c80f89303f930dbdf13
sayeed <sayeed0209@gmail.com>
2022-11-27 18:14:35 +0100

    router setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import SingleProduct from './pages/SingleProduct';
+import Products from './pages/Products';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <main>
+        <Routes>
+          {['/', 'products'].map((path, index) => (
+            <Route path={path} element={<Products />} key={index} />
+          ))}
+          <Route path="/:id" element={<SingleProduct />} />
+        </Routes>
+      </main>
+    </Router>
   );
 }
 
