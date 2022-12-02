09e2d13c037739d2f8ec7d6b14bee87c43136512
sayeed <sayeed0209@gmail.com>
2022-12-02 18:25:12 +0100

    Error page component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import SingleProduct from './pages/SingleProduct';
-import Products from './pages/Products';
-import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
+import SingleProduct from "./pages/SingleProduct";
+import Products from "./pages/Products";
+import Navbar from "./components/Navbar";
+import Error from "./pages/ErrorPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
@@ -11,10 +12,11 @@ function App() {
       <Navbar />
       <main>
         <Routes>
-          {['/', 'products'].map((path, index) => (
+          {["/", "products"].map((path, index) => (
             <Route path={path} element={<Products />} key={index} />
           ))}
           <Route path="/:id" element={<SingleProduct />} />
+          <Route path="*" element={<Error />} />
         </Routes>
       </main>
     </Router>
