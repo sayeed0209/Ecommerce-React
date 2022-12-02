82459f5fa11aa4c9435bef6c2b917fb8a52f769e
sayeed <sayeed0209@gmail.com>
2022-12-02 19:58:20 +0100

    removed error page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "bootstrap/dist/css/bootstrap.css";
 import SingleProduct from "./pages/SingleProduct";
 import Products from "./pages/Products";
 import Navbar from "./components/Navbar";
-import Error from "./pages/ErrorPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
@@ -16,7 +15,6 @@ function App() {
             <Route path={path} element={<Products />} key={index} />
           ))}
           <Route path="/:id" element={<SingleProduct />} />
-          <Route path="*" element={<Error />} />
         </Routes>
       </main>
     </Router>
