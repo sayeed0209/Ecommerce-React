8b16eb55049c84e7dba54d53e8ec19c7d69c59a6
sayeed <sayeed0209@gmail.com>
2022-12-02 18:26:51 +0100

    Sending product data to store in the cart data

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -1,31 +1,38 @@
-import React, { useState, useEffect } from 'react';
-import { useProductContext } from '../context/products_context';
-import CartColors from './CartColors';
-import CartStorage from './CartStorage';
+import React, { useState, useEffect } from "react";
+import { useProductContext } from "../context/products_context";
+import CartColors from "./CartColors";
+import CartStorage from "./CartStorage";
 const AddToCart = ({ product }) => {
-  const [selectedColor, setSelectedColor] = useState('');
-  const [selectedStorage, setSelectedStorage] = useState('');
+  const [selectedColorCode, setSelectedColorCode] = useState(null);
+  const [selectedStorageCode, setSelectedStorageCode] = useState(null);
+  const [selectedColor, setSelectedColor] = useState(null);
+  const [selectedStorage, setSelectedStorage] = useState(null);
   const [colors, setColors] = useState([]);
   const [storage, setStorage] = useState([]);
   const { cartHandler } = useProductContext();
+  let { model, brand, imgUrl, price, weight, cpu, ram } = product;
+  let finalObj = { model, brand, imgUrl, price, weight, cpu, ram };
   useEffect(() => {
     if (product != null && product.options != null) {
       const { colors, storages } = product.options;
       setColors(colors);
       setStorage(storages);
       if (colors.length === 1) {
-        setSelectedColor(colors[0].code);
+        setSelectedColorCode(colors[0].code);
+        setSelectedColor(colors[0].name);
       } else {
-        setSelectedColor(colors[0].code);
+        setSelectedColorCode(colors[0].code);
+        setSelectedColor(colors[0].name);
       }
       if (storages.length === 1) {
-        setSelectedStorage(storages[0].code);
+        setSelectedStorageCode(storages[0].code);
+        setSelectedStorage(storages[0].name);
       } else {
-        setSelectedStorage(storages[0].code);
+        setSelectedStorageCode(storages[0].code);
+        setSelectedStorage(storages[0].name);
       }
     }
   }, [product]);
-
   return (
     <div>
       <div className="color d-flex align-items-center">
@@ -35,10 +42,11 @@ const AddToCart = ({ product }) => {
             <div key={idx}>
               <CartColors
                 color={color}
-                selectedColor={selectedColor}
+                selectedColor={selectedColorCode}
                 onClick={() => {
-                  if (colors.length > 1 && color.code !== selectedColor) {
-                    setSelectedColor(color.code);
+                  if (colors.length > 1 && color.code !== selectedColorCode.code) {
+                    setSelectedColorCode(color.code);
+                    setSelectedColor(color.name);
                   }
                 }}
               />
@@ -53,11 +61,12 @@ const AddToCart = ({ product }) => {
             <div key={i} className="d-flex align-items-center">
               <CartStorage
                 space={space}
-                selectedStorage={selectedStorage}
+                selectedStorage={selectedStorageCode}
                 name={space.name}
                 onClick={() => {
-                  if (storage.length > 1 && space.code !== selectedStorage) {
-                    setSelectedStorage(space.code);
+                  if (storage.length > 1 && space.code !== selectedStorageCode) {
+                    setSelectedStorageCode(space.code);
+                    setSelectedStorage(space.name);
                   }
                 }}
               />
@@ -67,7 +76,16 @@ const AddToCart = ({ product }) => {
       </div>
       <button
         className="btn btn-outline-success mt-3 mb-3"
-        onClick={() => cartHandler(product.id, selectedColor, selectedStorage)}
+        onClick={() =>
+          cartHandler(
+            product.id,
+            selectedColorCode,
+            selectedStorageCode,
+            finalObj,
+            selectedColor,
+            selectedStorage,
+          )
+        }
       >
         Add to Cart
       </button>
